0765f0a3a11a7ab58f967155784da8e1dd13b98d
Thrivikrama Sarma Bharathula <bhvs.thrivikram16@gmail.com>
2024-02-14 20:35:45 +0530

    Login files added and curser drawable changed

---
+++ app/src/main/java/com/intern/xtrade/LoginOTPVerification.kt
@@ -0,0 +1,11 @@
+package com.intern.xtrade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginOTPVerification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_otpverification)
+    }
+}
